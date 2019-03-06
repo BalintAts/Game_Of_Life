a7c61034f778406270c81c31194a3dd7e5ffab81
Balint Ats <atsbalint@gmail.com>
2019-03-06 21:28:02 +0100

    rules now work

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import copy
 import os
+import time
 from file_handler import make_content_from_file
 from ui import display
 
 clear = lambda: os.system('clear')
 
 content = (make_content_from_file("gameoflife/Game_Of_Life/random_like_init.txt"))
-#content = (make_content_from_file("gameoflife/Game_Of_Life/glider.txt"))
+content = (make_content_from_file("gameoflife/Game_Of_Life/glider.txt"))
 
 #display(content)
 #print(content)
@@ -18,9 +20,11 @@ Any live cell with two or three live neighbours lives on to the next generation.
 Any live cell with more than three live neighbours dies, as if by overpopulation.
 Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.'''
 
-def step(content):    
+def step(content): 
+    newcontent =  copy.deepcopy(content)  
     for i in range(1,len(content[:-1])):
         row_list = list(content[i])
+        new_rowlist = list(newcontent[i])
         for j in range(1,len(row_list[:-1])):
             living_neighbours = 0
             #print(content[i])
@@ -47,28 +51,28 @@ def step(content):
             #    print('exception' + str(living_neighbours))
                 pass
             if row_list[j] == '0' and living_neighbours < 2:
-                row_list[j] = '_'
+                new_rowlist[j] = '_'
             elif row_list[j] == '0' and 2 <= living_neighbours <= 3:
-                pass
+                new_rowlist[j] = '0'
             elif row_list[j] == '0' and living_neighbours > 3:
-                row_list[j] = '_'
+                new_rowlist[j] = '_'
             elif row_list[j] == '_' and living_neighbours == 3:
-                row_list[j] = '0'
+                new_rowlist[j] = '0'
 
-        content[i] = ''.join(row_list)    
-
-
+        newcontent[i] = ''.join(new_rowlist)    
 
 
 
-    return content
+
+
+    return newcontent
 
 
         
         
 while True:
     display(content)        
-    input('press enter')
+    time.sleep(.05)
     content = step(content)
     clear()
     
